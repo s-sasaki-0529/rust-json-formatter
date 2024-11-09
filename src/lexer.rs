f8a938b4d2638e63ae448ff803ce6597828190af
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-11-10 01:06:54 +0900

    [字句解析] 字句解析作業用の構造体を定義

--- src/lexer.rs
+++ src/lexer.rs
@@ -12,3 +12,10 @@ pub enum Token {
     False,          // false
     Null,           // null
 }
+
+pub struct Lexer<'a> {
+    input: &'a str,       // 字句解析対象の文字列全体
+    position: usize,      // 解析中の現在の文字位置
+    read_position: usize, // 解析中の次の文字位置
+    ch: Option<char>,     // 現在解析中の文字 (None は EOF)
+}
