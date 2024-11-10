c3c317dcff290aaa8c69e0680a3125531edbc0a7
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-11-10 17:33:52 +0900

    [chore] JSONモジュールを切り出しておく

--- src/parser.rs
+++ src/parser.rs
@@ -1,19 +1,8 @@
-use crate::lexer::{Lexer, Token};
+use crate::{
+    json::{JsonArray, JsonObject, JsonValue},
+    lexer::{Lexer, Token},
+};
 use std::collections::HashMap;
-
-#[derive(Debug, PartialEq)]
-pub enum JsonValue {
-    Object(JsonObject), // {"key": "value"}
-    Array(JsonArray),   // [1, 2, 3]
-    String(String),     // "hello, world"
-    Number(f64),        // 123.456
-    True,               // true
-    False,              // false
-    Null,               // null
-}
-
-pub type JsonObject = HashMap<String, JsonValue>;
-pub type JsonArray = Vec<JsonValue>;
 
 pub struct Parser<'a> {
     lexer: Lexer<'a>,
