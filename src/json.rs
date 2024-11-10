3ebeba74160ceec4c03b201ed9d1be5bc639df83
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-11-10 20:03:11 +0900

    [Formatter] Object を実装する

--- src/json.rs
+++ src/json.rs
@@ -30,10 +30,28 @@ impl JsonValue {
      */
     fn format_value(&self, indent: usize, formatted: &mut String) {
         match self {
-            JsonValue::Object(obj) => {}
+            JsonValue::Object(obj) => {
+                self.push_str(formatted, "{\n");
+                for (i, (key, value)) in obj.iter().enumerate() {
+                    self.push_indent(formatted, indent + 2);
+                    self.push_str(formatted, "\"");
+                    self.push_str(formatted, key);
+                    self.push_str(formatted, "\"");
+                    self.push_str(formatted, ": ");
+                    value.format_value(indent + 2, formatted);
+                    if i < obj.len() - 1 {
+                        self.push_str(formatted, ",\n");
+                    } else {
+                        self.push_str(formatted, "\n");
+                    }
+                }
+                self.push_indent(formatted, indent);
+                formatted.push_str("}")
+            }
             JsonValue::Array(array) => {
-                self.push_str_with_indent(formatted, indent, "[\n");
+                self.push_str(formatted, "[\n");
                 for (i, value) in array.iter().enumerate() {
+                    self.push_indent(formatted, indent + 2);
                     value.format_value(indent + 2, formatted);
                     if i < array.len() - 1 {
                         self.push_str(formatted, ",\n");
@@ -51,16 +69,16 @@ impl JsonValue {
             }
             JsonValue::Number(num) => {
                 let value = &num.to_string();
-                self.push_str_with_indent(formatted, indent, value);
+                self.push_str(formatted, value);
             }
             JsonValue::True => {
-                self.push_str_with_indent(formatted, indent, "true");
+                self.push_str(formatted, "true");
             }
             JsonValue::False => {
-                self.push_str_with_indent(formatted, indent, "false");
+                self.push_str(formatted, "false");
             }
             JsonValue::Null => {
-                self.push_str_with_indent(formatted, indent, "null");
+                self.push_str(formatted, "null");
             }
         }
     }
@@ -156,6 +174,81 @@ mod tests {
     ]
   ]
 ]"#;
+        assert_eq!(value.format(0), expected);
+    }
+
+    #[test]
+    fn test_format_value_object() {
+        let mut object = IndexMap::new();
+        object.insert("key1".to_string(), JsonValue::Number(123.0));
+        object.insert("key2".to_string(), JsonValue::String("value".to_string()));
+        let value = JsonValue::Object(object);
+        let expected = r#"{
+  "key1": 123,
+  "key2": "value"
+}"#;
+        assert_eq!(value.format(0), expected);
+    }
+
+    #[test]
+    fn test_format_value_object_nested() {
+        let mut object = IndexMap::new();
+        let mut nested_object = IndexMap::new();
+        nested_object.insert("key3".to_string(), JsonValue::Number(456.0));
+        nested_object.insert("key4".to_string(), JsonValue::Number(789.0));
+        object.insert("key1".to_string(), JsonValue::Number(123.0));
+        object.insert("key2".to_string(), JsonValue::Object(nested_object));
+
+        let value = JsonValue::Object(object);
+        let expected = r#"{
+  "key1": 123,
+  "key2": {
+    "key3": 456,
+    "key4": 789
+  }
+}"#;
+        assert_eq!(value.format(0), expected);
+    }
+
+    fn test_format_value_mixed() {
+        let mut object = IndexMap::new();
+        let mut nested_object1 = IndexMap::new();
+        nested_object1.insert("key2".to_string(), JsonValue::Number(2.0));
+        nested_object1.insert("key3".to_string(), JsonValue::Number(3.0));
+
+        let mut nested_object2 = IndexMap::new();
+        nested_object2.insert(
+            "key5".to_string(),
+            JsonValue::Array(vec![JsonValue::Number(5.0), JsonValue::Number(6.0)]),
+        );
+
+        object.insert(
+            "key1".to_string(),
+            JsonValue::Array(vec![
+                JsonValue::Number(1.0),
+                JsonValue::Object(nested_object1),
+                JsonValue::Array(vec![JsonValue::Number(4.0), JsonValue::Object(nested_object2)]),
+            ]),
+        );
+        let value = JsonValue::Object(object);
+        let expected = r#"{
+  "key1": [
+    1,
+    {
+      "key2": 2
+      "key3": 3
+    },
+    [
+      4,
+      {
+        "key5": [
+          5,
+          6
+        ]
+      }
+    ]
+  ],
+}"#;
         assert_eq!(value.format(0), expected);
     }
 }
