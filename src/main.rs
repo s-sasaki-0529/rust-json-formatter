8c495e7eabe37b3c3fbb62cf34aabfa8b735e26b
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-11-10 20:36:18 +0900

    Main 関数にて、標準入力から受け取ったJSON文字列をフォーマットして出力させる

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,22 @@ mod json;
 mod lexer;
 mod parser;
 
-use lexer::Token;
+use lexer::Lexer;
+use parser::Parser;
+use std::io::{self, Read};
 
 fn main() {
-    println!("JSON Formatter in Rust");
+    // 標準入力からJSON文字列を読み込む
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("テキストの読み込みに失敗しました");
+
+    // 字句解析+構文解析
+    let lexer = Lexer::new(&input);
+    let mut parser = Parser::new(lexer);
+    let json = parser.parse().expect("JSONのパースに失敗しました");
+
+    // パース結果を標準出力
+    println!("{}", json.format(0));
 }
