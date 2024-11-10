12069486e34e94460297fd53a03da6e5efdf0273
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-11-10 15:07:26 +0900

    [構文解析] JSON文字列内のリテラル値をパースできるようにする

--- src/parser.rs
+++ src/parser.rs
@@ -34,6 +34,60 @@ impl<'a> Parser<'a> {
     }
 
     /**
+     * JSON全体をパースする
+     */
+    pub fn parse(&mut self) -> Option<JsonValue> {
+        self.parse_value()
+    }
+
+    /**
+     * JSON値をパースする
+     */
+    fn parse_value(&mut self) -> Option<JsonValue> {
+        match &self.current_token {
+            Some(Token::LeftBrace) => self.parse_object(),  // { がオブジェクトの開始
+            Some(Token::LeftBracket) => self.parse_array(), // [ が配列の開始
+            Some(Token::String(string)) => {
+                let cloned_string = string.clone();
+                self.next_token();
+                Some(JsonValue::String(cloned_string))
+            }
+            Some(Token::Number(number)) => {
+                let copied_number = *number;
+                self.next_token();
+                Some(JsonValue::Number(copied_number))
+            }
+            Some(Token::True) => {
+                self.next_token();
+                Some(JsonValue::True)
+            }
+            Some(Token::False) => {
+                self.next_token();
+                Some(JsonValue::False)
+            }
+            Some(Token::Null) => {
+                self.next_token();
+                Some(JsonValue::Null)
+            }
+            _ => None,
+        }
+    }
+
+    /**
+     * オブジェクトをパースする
+     */
+    fn parse_object(&mut self) -> Option<JsonValue> {
+        None
+    }
+
+    /**
+     * 配列をパースする
+     */
+    fn parse_array(&mut self) -> Option<JsonValue> {
+        None
+    }
+
+    /**
      * 次のトークンを取得する
      */
     fn next_token(&mut self) {
@@ -69,5 +123,23 @@ mod tests {
         assert_eq!(parser.current_token, Some(Token::RightBrace));
         parser.next_token();
         assert_eq!(parser.current_token, None);
+    }
+
+    #[test]
+    fn test_parse_string_simple() {
+        let mut parser1 = Parser::new(Lexer::new(r#""Hello, World!""#));
+        assert_eq!(parser1.parse(), Some(JsonValue::String("Hello, World!".to_string())));
+
+        let mut parser2 = Parser::new(Lexer::new(r#"-123.1"#));
+        assert_eq!(parser2.parse(), Some(JsonValue::Number(-123.1)));
+
+        let mut parser3 = Parser::new(Lexer::new(r#"true"#));
+        assert_eq!(parser3.parse(), Some(JsonValue::True));
+
+        let mut parser4 = Parser::new(Lexer::new(r#"false"#));
+        assert_eq!(parser4.parse(), Some(JsonValue::False));
+
+        let mut parser5 = Parser::new(Lexer::new(r#"null"#));
+        assert_eq!(parser5.parse(), Some(JsonValue::Null));
     }
 }
